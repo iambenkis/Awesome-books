95c7f0cceb8bfb8c1656e4969aa0319272db455b
benjamin kisenge <benkisenge03@gmail.com>
2022-08-01 12:16:26 +0200

    Create display book function

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const removeBtn = document.querySelector('.remove');
 const addBtn = document.querySelector('.add');
 const inTitle = document.querySelector('#in-title');
 const inAuthor = document.querySelector('#in-author');
-
-const books = [
+const bookContainer = document.querySelector('.books');
+const store = [
     {
         title:'the law',
         author: 'benk',
@@ -14,4 +14,18 @@ const books = [
         title:'the book1',
         author: 'benk',
     }
-]
+];
+
+const displayBook = (store) => {
+    store.map(s => {
+        bookContainer.innerHTML += `
+            <div class="book-item">
+                <p class="title">${s.title}it</p>
+                <p class="Autor">${s.author}</p>
+                <button class="remove">Remove</button>
+            </div> 
+        `
+    });
+}
+
+displayBook(store);
