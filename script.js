98b1b00bba5da6a1432c02609025b69947893b7a
benjamin kisenge <benkisenge03@gmail.com>
2022-08-01 12:09:52 +0200

    Create js variable

---
+++ script.js
@@ -0,0 +1,17 @@
+const title = document.querySelectorAll('.title');
+const author = document.querySelectorAll('.author');
+const removeBtn = document.querySelector('.remove');
+const addBtn = document.querySelector('.add');
+const inTitle = document.querySelector('#in-title');
+const inAuthor = document.querySelector('#in-author');
+
+const books = [
+    {
+        title:'the law',
+        author: 'benk',
+    },
+    {
+        title:'the book1',
+        author: 'benk',
+    }
+]
