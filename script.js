8b361b4f1d06f1299abe887a5f0390e98b608421
AzizRAA <azizbekruzmatovraa@gmail.com>
2022-08-02 17:51:25 +0300

    added localStorage

--- script.js
+++ script.js
@@ -1,60 +1,80 @@
 const addBtn = document.querySelector('.add');
-let inTitle = document.querySelector('#in-title');
-let inAuthor = document.querySelector('#in-author');
-const bookContainer = document.querySelector('.books'); 
-let removeBtn =  document.querySelectorAll('.remove');
-
+const inTitle = document.querySelector('#in-title');
+const inAuthor = document.querySelector('#in-author');
+const bookContainer = document.querySelector('.books');
 
 let myLib = [];
 
+// save to localStorage
 
-function Book (title,author) {
-    this.title = title;
-    this.author = author;
+function saveMyLib() {
+  const str = JSON.stringify(myLib);
+  localStorage.setItem('myLib', str);
 }
 
-Book.prototype.saveBook = function () {
-    let savedBook = {
-        title : this.title,
-        author :this.author
-    }
-    
-    myLib.unshift(savedBook);
-    displayBook ();
-}
-    
-function addBook (e){
-    e.preventDefault();
-    let bookTitle = inTitle.value;
-    let bookAuthor = inAuthor.value;
-    bookContainer.innerHTML = '';
-    if(bookAuthor!== '' && bookTitle!== '') { 
-        let insertBook = new Book (bookTitle,bookAuthor);
-        insertBook.saveBook();
-    }
+// get from localStorage
+
+function getMyLib() {
+  const str = localStorage.getItem('myLib');
+  myLib = JSON.parse(str);
+  if (!myLib) {
+    myLib = [];
+  }
 }
 
-function displayBook (){ 
-    bookContainer.innerHTML = '';
-    myLib.forEach((s) => {
+function displayBook() {
+  getMyLib();
+  bookContainer.innerHTML = '';
+  myLib.forEach((s) => {
     bookContainer.innerHTML += ` 
             <div class="book-item">
                 <p class="title">${s.title}</p>
-                <p class="Autor">${s.author}</p>
+                <p class="Author">${s.author}</p>
                 <button class="remove">Remove</button>
             </div>
-    `; 
-}); 
+    `;
+  });
 
-let deleteButton = bookContainer.querySelectorAll('.remove');  
-deleteButton.forEach((key,index) => key.addEventListener('click', ()=> {
+  function deleteFunc(index) {
+    myLib.splice(index, 1);
+    saveMyLib();
+    displayBook();
+  }
+
+  const deleteButton = bookContainer.querySelectorAll('.remove');
+  deleteButton.forEach((key, index) => key.addEventListener('click', () => {
     deleteFunc(index);
-}))   
+  }));
 }
 
-function deleteFunc (index) {
-myLib.splice(index,1);
-displayBook();
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+Book.prototype.saveBook = function () {
+  const savedBook = {
+    title: this.title,
+    author: this.author,
+  };
+
+  myLib.unshift(savedBook);
+  displayBook();
+};
+
+function addBook(e) {
+  e.preventDefault();
+  const bookTitle = inTitle.value;
+  const bookAuthor = inAuthor.value;
+  if (inTitle.value !== '' && inAuthor.value !== '') {
+    bookContainer.innerHTML = '';
+    const insertBook = new Book(bookTitle, bookAuthor);
+    insertBook.saveBook();
+    inAuthor.value = '';
+    inTitle.value = '';
+    myLib.push(insertBook);
+    saveMyLib();
+  }
 }
 
 displayBook();
