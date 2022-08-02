2aa648f41fac450306990e5dcfc1150f03334993
benjamin kisenge <benkisenge03@gmail.com>
2022-08-02 11:51:46 +0200

    Declare variable

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-const title = document.querySelectorAll('.title');
-const author = document.querySelectorAll('.author');
-const removeBtn = document.querySelector('.remove');
 const addBtn = document.querySelector('.add');
-const inTitle = document.querySelector('#in-title');
-const inAuthor = document.querySelector('#in-author');
-const bookContainer = document.querySelector('.books');
-const store = [
-    {
-        title:'the law',
-        author: 'benk',
-    },
-    {
-        title:'the book1',
-        author: 'benk',
-    }
-];
-
-const displayBook = (store) => {
-    store.map(s => {
-        bookContainer.innerHTML += `
-            <div class="book-item">
-                <p class="title">${s.title}it</p>
-                <p class="Autor">${s.author}</p>
-                <button class="remove">Remove</button>
-            </div> 
-        `
-    });
-}
-
-displayBook(store);
+let inTitle = document.querySelector('#in-title');
+let inAuthor = document.querySelector('#in-author');
+const bookContainer = document.querySelector('.books'); 
+let removeBtn =  document.querySelectorAll('.remove');
